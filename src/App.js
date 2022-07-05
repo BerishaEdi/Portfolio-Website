d6e40102b138551ac939fea0b488130706703f83
berishaedi <berisha.edi@outlook.com>
2022-07-06 01:04:06 +0200

    2.0

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import AboutMe from './components/AboutMe';
-import CV from './components/CV';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Skills from './components/Skills';
