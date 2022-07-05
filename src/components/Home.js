3463249a8871ec36289a69cc9d02db5e7c6f2894
berishaedi <berisha.edi@outlook.com>
2022-07-05 20:15:44 +0200

    secon

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import portrait from "../images/portrait1.png"
+import portrait from "../images/portrait1.pn"
 
 const Home = () => {
   return (
         <div className='home'>
             <h1 className='titel'> Eduard <br /> Berisha</h1>
-            <img src={portrait} alt="portrait" className='bild '></img>
+            {/*<img src={portrait} alt="portrait" className='bild '></img>*/}
         </div>
   )
 }
