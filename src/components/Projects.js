a269475a342dfc157972f872663a639c21fc6ba4
berishaedi <berisha.edi@outlook.com>
2022-07-06 20:44:54 +0200

    2.3

---
+++ src/components/Projects.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div>
+        <div className='calculatorDiv'>
+            <a className='calculatorLink' href="https://portfolio.bbbaden.ch/user/e-berisha-inf20/la-1305-react-nativ-taschenrechner">React-Native Taschenrechner</a>
+        </div>
+    </div>
+  )
+}
+
+export default Projects
