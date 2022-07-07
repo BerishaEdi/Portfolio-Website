0f44fe75cfead05cc9cc9ea57a22101d60816b91
berishaedi <berisha.edi@outlook.com>
2022-07-07 18:57:44 +0200

    3.0

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import calculatorImg from "../images/calculator.png"
 
 const Projects = () => {
   return (
     <div>
-        <div className='calculatorDiv'>
+        <h1 className='titelProjects'>Projekte</h1>
+        <div class="grid">
+
+          <div class="div1"> <a className='calculatorLink' href="https://portfolio.bbbaden.ch/user/e-berisha-inf20/la-1305-react-nativ-taschenrechner">React-Native Taschenrechner</a>
+            <img src={calculatorImg} alt="CalculatorIMG" width="400" height="740" className='calculatorimg'></img>
+          </div>
+
+          <div class="div2"> 
             <a className='calculatorLink' href="https://portfolio.bbbaden.ch/user/e-berisha-inf20/la-1305-react-nativ-taschenrechner">React-Native Taschenrechner</a>
+          </div>
+
+          <div class="div3">  
+            <a className='calculatorLink' href="https://portfolio.bbbaden.ch/user/e-berisha-inf20/la-1305-react-nativ-taschenrechner">React-Native Taschenrechner</a>
+          </div>
+
+          <div class="div4">  
+            <a className='calculatorLink' href="https://portfolio.bbbaden.ch/user/e-berisha-inf20/la-1305-react-nativ-taschenrechner">React-Native Taschenrechner</a>
+          </div>
+
         </div>
     </div>
   )
