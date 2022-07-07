b2f5222dd5842fc230d20ea854bdee6ad46a5f37
berishaedi <berisha.edi@outlook.com>
2022-07-07 20:55:30 +0200

    3.0

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,18 +10,19 @@ const Projects = () => {
         <h1 className='titelProjects'>Projekte</h1>
         <div class="grid">
 
-          <div class="div1"> <a className='calculatorLink' href="https://portfolio.bbbaden.ch/user/e-berisha-inf20/la-1305-react-nativ-taschenrechner">React-Native <br></br> Taschenrechner</a>
+          <div class="div1"> 
+            <a className='calculatorLink' href="https://portfolio.bbbaden.ch/user/e-berisha-inf20/la-1305-react-nativ-taschenrechner">React-Native <br></br> Taschenrechner</a>
             <img src={calculatorImg} alt="CalculatorIMG" width="50%" height="auto" className='calculatorimg'></img>
           </div>
 
           <div class="div2"> 
             <a className='calculatorLink' href="https://portfolio.bbbaden.ch/user/e-berisha-inf20/la-1305-react-nativ-taschenrechner">Java <br></br> Platformer</a>
-            <img src={platformerImg} alt="platformerIMG" width="80%" height="auto" className='platformerimg'></img>
+            <img src={platformerImg} alt="platformerIMG" width="90%" height="auto" className='platformerimg'></img>
           </div>
 
           <div class="div3">  
             <a className='calculatorLink' href="https://portfolio.bbbaden.ch/user/e-berisha-inf20/la-1305-react-nativ-taschenrechner">Discord <br></br> Bot</a>
-            <img src={botImg} alt="botIMG" width="80%" height="auto" className='botimg'></img>
+            <img src={botImg} alt="botIMG" width="90%" height="auto" className='botimg'></img>
           </div>
 
         </div>
